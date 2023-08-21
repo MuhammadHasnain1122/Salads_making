fc5d6d4ea9dca32d316dc8c24df8fd31cd6bb527
MuhammadHasnain1122 <aliVeaDDz8@gmail.com>
2023-08-21 10:42:22 +0500

    recent changes

--- app/api/[resource]/route.js
+++ app/api/[resource]/route.js
@@ -1,92 +1,74 @@
-import {NextResponse} from 'next/server';
-import {writeFile} from 'fs/promises';
+import { NextResponse } from 'next/server';
+import { readFile, writeFile } from 'fs/promises';
 
-
-
-// Routes
-export async function GET(request, {params}) {
+// GET request handler
+export async function GET(request, { params }) {
   const response = await getData(params?.resource);
 
   return NextResponse.json(
-    response ? response : {error: "Unknown resource"},
-    {status: response ? 200 : 404}
-  );
-}
-
-
-export async function POST(request, {params}) {
-  debugger
-  const {resource} = params;
-  const isReset = resource === 'reset';
-  const action = isReset ? resetSavedData : saveData;
-
-  const body = await request.json();
-
-  const response = await (action(resource, body)).then(async r => {
-    return await getData(resource);
-  });
-
-  return NextResponse.json(
-    response ? response : {error: "Unknown resource"},
+    response ? response : { error: "Unknown resource" },
     { status: response ? 200 : 404 }
   );
 }
 
+// POST request handler
+export async function POST(request, { params }) {
+  const { resource } = params;
+  const action = saveData; // Always use saveData
 
-const getData = async (resource) => {
-  return await new Promise(async (resolve, reject) => {
-    let savedData = await import('@/data/savedData.json');
+  const body = await request.json();
 
-    const firstLoad = !!Object.entries(savedData).length;
-    const data = !firstLoad ? savedData : await import('@/data/initialData.json');
+  await action(resource, [body]); // Wrap body in an array and perform the saveData action
 
-    if (firstLoad) await saveDataCopy(data);
-
-    console.log({data, resource, r: data[resource]})
-
-    resolve(resource && resource !== 'reset' ? data[resource] : data);
-  });
-};
-
-
-
-const saveData = async (resource, data) => {
-  data = Array.isArray(data) ? data : [data];
-
-  // Load
-  const newData = {...await getData()};
-  if (!Object.keys(newData).includes(resource)) return;
-
-  const resourceData = newData[resource];
-
-  // Insert
-  data.forEach(item => {
-    let newItem = resourceData.find(i => i.id === item.id);
-
-    if (newItem) {
-      resourceData[resourceData.indexOf(newItem)] = {...newItem, ...item};
-    } else {
-      const newId = Math.max(...resourceData.map(i => i.id)) + 1;
-      const {id, ...rest} = item;
-      newItem = {id: newId, ...rest};
-      resourceData.push(newItem);
-    }
-  });
-
-  newData[resource] = resourceData;
-
-  // Write
-  await writeFile('./src/data/savedData.json', JSON.stringify(newData, null, "\t"));
-};
-
-const saveDataCopy = async (data) => {
-  await writeFile('./src/data/savedData.json', JSON.stringify(data, null, "\t"));
+  return NextResponse.json(
+    { message: "Data successfully saved" },
+    { status: 200 }
+  );
 }
 
-// const saveDataCopyInitial = async (data) => {
-//   await writeFile('./src/data/initialData.json', JSON.stringify(data, null, "\t"));
-// }
+// Functions
 
-// const resetSavedData = async () => {
-//   await writeFile(`./src/data/savedData.json`, JSON.stringify({}));
-// }
+const getData = async (resource) => {
+  try {
+    const dataBuffer = await readFile('./src/data/savedData.json');
+    const data = JSON.parse(dataBuffer.toString());
+
+    return resource && resource !== 'reset' ? data[resource] : data;
+  } catch (error) {
+    console.error('Error getting data:', error);
+    return null;
+  }
+};
+
+const saveData = async (resource, newData) => {
+  try {
+    const dataBuffer = await readFile('./src/data/savedData.json');
+    const data = JSON.parse(dataBuffer.toString());
+
+    if (!Object.keys(data).includes(resource)) {
+      data[resource] = [];
+    }
+
+    const resourceData = data[resource];
+
+    newData.forEach(item => {
+      let existingItem = resourceData.find(i => i.id === item.id);
+
+      if (existingItem) {
+        Object.assign(existingItem, item);
+      } else {
+        const newId = Math.max(...resourceData.map(i => i.id)) + 1;
+        const newItem = { id: newId, ...item };
+        resourceData.push(newItem);
+      }
+    });
+
+    await writeFile('./src/data/savedData.json', JSON.stringify(data, null, "\t"));
+  } catch (error) {
+    console.error('Error saving data:', error);
+  }
+};
+
+const resetSavedData = async () => {
+  await writeFile(`./src/data/savedData.json`, JSON.stringify({}));
+};
